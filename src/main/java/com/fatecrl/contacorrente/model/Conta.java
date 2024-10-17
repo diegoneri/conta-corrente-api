6b624baa156914b6c5b542ffdb5b3cc866b81359
Diego Neri <diego@diegoneri.com>
2024-10-17 20:18:45 -0300

    Adiciona entidades cliente, pf e pj, usando JOINED

--- src/main/java/com/fatecrl/contacorrente/model/Conta.java
+++ src/main/java/com/fatecrl/contacorrente/model/Conta.java
@@ -1,21 +1,12 @@
 package com.fatecrl.contacorrente.model;
 
-import java.io.Serializable;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_conta")
-public class Conta implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class Conta extends AbstractEntity  {
 	@Column(name = "nr_agencia", nullable = false)
 	private Integer agencia;
 	@Column(name = "nm_numero", nullable = false, length = 10)
@@ -29,15 +20,9 @@ public class Conta implements Serializable {
     }
 
     public Conta(Long id) {
-        this.id = id;
+        super(id);
     }    
     
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
     public Integer getAgencia() {
         return agencia;
     }
@@ -61,30 +46,5 @@ public class Conta implements Serializable {
     }
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Conta other = (Conta) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
     }
 }
---
+++ src/main/java/com/fatecrl/contacorrente/model/PessoaFisica.java
@@ -0,0 +1,38 @@
+package com.fatecrl.contacorrente.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoa_fisica")
+public class PessoaFisica extends Cliente {
+    private static final long serialVersionUID = 1L;
+    @Column(name = "cd_cpf", length = 11)
+    private String cpf;
+    @Column(name = "nm_profissao", length = 30)
+    private String profissao;
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(Long id) {
+        super(id);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
