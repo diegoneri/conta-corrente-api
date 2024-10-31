0fe09d7b9980864071c0148eb3303a882285c50c
Diego Neri <diego@diegoneri.com>
2024-10-31 20:43:42 -0300

    WIP

---
+++ src/main/java/com/fatecrl/contacorrente/dto/ContaCorrenteDTO.java
@@ -0,0 +1,23 @@
+package com.fatecrl.contacorrente.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Max;
+import lombok.Data;
+
+@Data
+public class ContaCorrenteDTO {
+	@NotNull
+    private Integer agencia;
+    @NotNull
+    @Size(min = 5, max = 5)
+	private String numero;
+    @NotNull
+    @Size(min = 4, max = 60)
+	private String titular;
+    @NotNull
+    @Min(1) 
+    @Max(3000)
+	private Double saldo;
+}
